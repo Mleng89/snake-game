64a244b3e899dddc6f75ee2beef8f767bc3b3132
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-11-01 18:16:13 -0400

    FEAT: Filling snake object

--- snake.py
+++ snake.py
@@ -1,15 +1,25 @@
-class Snake(object):
+import cube
+
+
+class Snake:
     def __init__(self, color, position):
-        pass
+        self.color = color
+        self.head = cube[position]
 
     def move(self):
         ...
 
     def reset(self, position):
-        ...
+        self.head = cube(position)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.direction_X = 0
+        self.direction_Y = 1
 
     def addCube(self):
-        ...
+        tail = self.body[-1]
+        x, y = tail.d
 
     def draw(self, surface):
         ...
