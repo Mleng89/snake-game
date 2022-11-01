7517b6455be7b4f8995ac9a80c45973cb04ae47b
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-11-01 18:11:53 -0400

    FEAT: Filling cube object

--- cube.py
+++ cube.py
@@ -1,9 +1,17 @@
 class Cube(object):
-    def __init__(self, start, directionX=1, directionY=0, color=(255, 0, 0)):
-        ...
+    def __init__(self, start, direction_X=1, direction_Y=0, color=(255, 0, 0)):
+        self.start = start
+        self.direction_X = direction_X
+        self.direction_Y = direction_Y
+        self.color = color
 
-    def move(self, directionX, directionY):
-        ...
+    def move(self, direction_X, direction_Y):
+        self.direction_X = direction_X
+        self.direction_Y = direction_Y
+        self.position = (
+            self.position[0] + self.direction_X,
+            self.position[1] + self.direction_Y,
+        )
 
     def draw(self, surface, eyes=False):
         ...
